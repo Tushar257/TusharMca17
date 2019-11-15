8d697280e51e261ffac25fe78fb95c7dcb65b23c
Tushar <31645535+Tushar257@users.noreply.github.com>
2019-11-15 13:52:50 +0530

    Update server.js

--- WeatherApp Ang-Node/server.js
+++ WeatherApp Ang-Node/server.js
@@ -1,5 +1,41 @@
 var express = require('express'); 
+var neode = require('neode');
+const instance = new neode('bolt://localhost:7687','WeatherAppDatabase', '1234');
+instance.model('WeatherDetails' , {
+    cityName : {
+        type : 'string'
+    },
+    tempToday :
+    {
+        type : 'string'
+    },
+    tempMinToday :
+    {
+        type : 'string'
+    },
+    tempMaxToday :
+    {
+        type : 'string'
+    },
+    tempMaxTom :{
+        type : 'string'
+    },
+    tempMinTom : {
+        type : 'string'
+    },
 
+    tempTom :{
+        type : 'string'
+    }
+});
+//instance.create('WeatherDetails', {
+
+
+
+
+
+
+})
 var axios = require("axios");
 var app = express();
 var bodyParser =require('body-parser');
@@ -10,7 +46,7 @@ app.get('/',(req,res)=>{
     res.sendFile("index.html");
 })
 app.post('/',(req,res)=>{
-    console.log("recieved");
+    console.log("received");
     console.log(req.body);
     res.send("ok")
 })
@@ -19,17 +55,22 @@ app.post("/city", function( req, res ) {
     var city = req.body.cityInput;
     
     //axios.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=31e69808cd436821a5e2fba6f494b787&units=metric")
-    let resp = axios.get("https://api.openweathermap.org/data/2.5/forecast?q=London&appid=31e69808cd436821a5e2fba6f494b787&units=metric")
+    var resp = axios.get("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=31e69808cd436821a5e2fba6f494b787&units=metric")
     .then(response => {
         //res.render("cityweather", { weather : response.data, city : city } );
         console.log(response.data);
-        //res.send(response.data);
+        res.send(response.data);
     })
     .catch(error => {
-        //console.log(error);
-        res.render( "cities", {merror : true} );
+        console.log(error);
+        //res.render( "cities", {merror : true} );
     });
+
+
+
+
+
 });
 app.listen(8080,()=>{
-    console.log("runninhg");
+    console.log("Server Listen at port number : 8080");
 });
