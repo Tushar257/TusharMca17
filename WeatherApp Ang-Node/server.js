2b00a45265a03c3b59e6f46f5add9c0520e9a835
Tushar <31645535+Tushar257@users.noreply.github.com>
2019-11-17 22:12:08 +0530

    Update server.js

--- WeatherApp Ang-Node/server.js
+++ WeatherApp Ang-Node/server.js
@@ -1,47 +1,40 @@
 var express = require('express'); 
 var neode = require('neode');
-const instance = new neode('bolt://localhost:7687','WeatherAppDatabase', '1234');
+const instance = new neode('bolt://localhost:7687','neo4j', '12345');
 instance.model('WeatherDetails' , {
     cityName : {
         type : 'string'
     },
     tempToday :
     {
-        type : 'string'
+        type : 'float'
     },
     tempMinToday :
     {
-        type : 'string'
+        type : 'float'
     },
     tempMaxToday :
     {
-        type : 'string'
+        type : 'float'
     },
     tempMaxTom :{
-        type : 'string'
+        type : 'float'
     },
     tempMinTom : {
-        type : 'string'
+        type : 'float'
     },
 
     tempTom :{
-        type : 'string'
+        type : 'flaot'
     }
 });
-//instance.create('WeatherDetails', {
-
-
-
-
-
-
-})
 var axios = require("axios");
 var app = express();
 var bodyParser =require('body-parser');
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
+app.set("view engine","ejs");
 app.get('/',(req,res)=>{
     res.sendFile("index.html");
 })
@@ -51,26 +44,74 @@ app.post('/',(req,res)=>{
     res.send("ok")
 })
 
-app.post("/city", function( req, res ) {
-    var city = req.body.cityInput;
     
-    //axios.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=31e69808cd436821a5e2fba6f494b787&units=metric")
-    var resp = axios.get("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=31e69808cd436821a5e2fba6f494b787&units=metric")
+app.post("/city", function( req, res ) {
+    var city = req.body.cityInput.toLowerCase();
+   
+        axios.get("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=31e69808cd436821a5e2fba6f494b787&units=metric")
     .then(response => {
-        //res.render("cityweather", { weather : response.data, city : city } );
-        console.log(response.data);
-        res.send(response.data);
+        
+            instance.all('WeatherDetails', {cityName: city})
+            .then(collection => {
+            const length = collection.length
+            if (length==0)
+            {
+
+            // INSERTING A NEW NODE IN THE DATABASE
+
+
+            instance.create('WeatherDetails', {
+            cityName :  city,
+            tempToday: response.data.list[0].main.temp,
+            tempMaxToday: response.data.list[0].main.temp_max,
+            tempMinToday: response.data.list[0].main.temp_min,
+            tempTom : response.data.list[6].main.temp,
+            tempMaxTom : response.data.list[6].main.temp_max,
+            tempMinTom: response.data.list[6].main.temp_min,
+            
+        
+        }).then(cityName=>
+            {   
+            console.log("DATABASE UPDATED WITH " + city.toUpperCase() + "'s DATA")
+            res.render("newcity",{weather : response.data , city:city});
+            }).catch(e =>{
+                console.log(e);
+            });
+        }
+
+        else
+        {   
+            const citydata = collection.get(0).get('cityName')            
+            const today = collection.get(0).get('tempToday')
+            const todaymin = collection.get(0).get('tempMinToday')
+            const todaymax = collection.get(0).get('tempMaxToday')
+            const tom = collection.get(0).get('tempTom')
+            const tommin = collection.get(0).get('tempMinTom')
+            const tommax = collection.get(0).get('tempMaxTom')
+            
+            /*DELETION OF DATA
+            instance.cypher('MATCH (n{cityName: {name}}) remove n.tempTom', {name: city})
+            */
+
+
+            /*UPDATION OF DATA
+            instance.cypher("MATCH (n { cityName:{name}})SET n.tempTom=25.23" ,{name: city})
+            */
+
+            res.render("cityweather", { todaytemp : today, city : citydata, todaymin : todaymin, todaymax : todaymax, tom : tom, tommin: tommin ,tommax:tommax  } );
+    
+    
+    
+    }
     })
-    .catch(error => {
+})
+
+      .catch(error => {
         console.log(error);
-        //res.render( "cities", {merror : true} );
     });
 
-
-
-
-
 });
+
 app.listen(8080,()=>{
-    console.log("Server Listen at port number : 8080");
+    console.log("Server Listening at port number : 8080");
 });
