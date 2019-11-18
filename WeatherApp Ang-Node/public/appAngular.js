ea768d50a1a76048a0c79f08dc019954db27c04b
Tushar <31645535+Tushar257@users.noreply.github.com>
2019-11-18 15:05:45 +0530

    Update appAngular.js

--- WeatherApp Ang-Node/public/appAngular.js
+++ WeatherApp Ang-Node/public/appAngular.js
@@ -1,14 +1,15 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope,$http) {
    $scope.submit= function(){
+      console.log("app")
       var data = JSON.stringify({
-            author: $scope.author,
-            title : $scope.title,
-            body : $scope.body
+            cityInput: $scope.cityName
         })
       console.log(data);
       $http.post("/city", data).then(function(data, status) {
+         console.log(data);
         console.log('Data posted successfully');
+        document.write(data.data);
       }).catch((err)=>{
           console.log("error");
           console.log(err);
